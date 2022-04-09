7742275f5e59949df594ed50dba256e6fb7d135b
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-04-09 15:24:57 -0400

    Implement more of TrackStarService

--- client/lib/trackstar_service.dart
+++ client/lib/trackstar_service.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+
+part 'trackstar_service.g.dart';
 
 class TrackStarService {
   final WebSocketChannel ws =
@@ -7,8 +11,14 @@ class TrackStarService {
 
   late String userName;
 
-  void createRoom() {
-    ws.sink.add(CreateRoomRequest(userName));
+  Future<void> createRoom() async {
+    ws.sink.add(jsonEncode(CreateRoomRequest(userName).toJson()));
+    CreateRoomResponse response =
+        CreateRoomResponse.fromJson(jsonDecode(await ws.stream.first));
+    if (!(response.topic == 'create_room_response' &&
+        response.status == 'success')) {
+      throw Error();
+    }
   }
 
   void shutdown() {
@@ -18,8 +28,21 @@ class TrackStarService {
 
 @JsonSerializable(fieldRename: FieldRename.snake)
 class CreateRoomRequest {
-  CreateRoomRequest(this.creatorName);
-
   final String topic = 'create_room';
   String creatorName;
+
+  CreateRoomRequest(this.creatorName);
+  Map<String, dynamic> toJson() => _$CreateRoomRequestToJson(this);
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class CreateRoomResponse {
+  final String topic;
+  final String status;
+  final String roomId;
+  final String creatorId;
+
+  CreateRoomResponse(this.topic, this.status, this.roomId, this.creatorId);
+  factory CreateRoomResponse.fromJson(Map<String, dynamic> json) =>
+      _$CreateRoomResponseFromJson(json);
 }
