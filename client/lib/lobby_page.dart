251c9c247981246cdaeaf5aafd1f58e75f1b15d8
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-06-02 00:09:04 -0400

    Fix lobby->game page transition to work when joining mid-round

--- client/lib/lobby_page.dart
+++ client/lib/lobby_page.dart
@@ -25,7 +25,7 @@ class _LobbyPageState extends State<LobbyPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (trackStarService.trackNumber == 1) {
+    if (trackStarService.trackNumber != -1) {
       Future.delayed(Duration.zero, () => navigateToGamePage());
     }
 
