1768c68e664fc8a30210eb6ea29c60f9450e99b1
JMS55 <47158642+JMS55@users.noreply.github.com>
2022-06-08 14:48:04 -0400

    Fullscreen on mobile

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'home_page.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
@@ -33,5 +39,18 @@ class App extends StatelessWidget {
         );
       }),
     );
+  }
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    SystemChrome.setSystemUIChangeCallback(
+      (_) => Future.delayed(
+        const Duration(milliseconds: 1200),
+        () => SystemChrome.restoreSystemUIOverlays(),
+      ),
+    );
+
+    super.initState();
   }
 }
